211a6530d8cdd897083df5e6a56e32724c8be7e7
Anna Arutyunova <anna.arutyunova8848@gmail.com>
2023-06-07 21:40:51 -0600

    add login and registration, add minor css

---
+++ routes/accountRoute.js
@@ -0,0 +1,20 @@
+/**************************************
+ * Account routes
+ * Unit 4, deliver login view activity
+ ***************************************/
+// Neede Resources
+const express = require("express")
+const router = new express.Router()
+const accountController = require("../controllers/accountController")
+const utilities = require("../utilities")
+
+/**************************************
+ * Deliver Login View
+ * Unit 4, deliver login view activity
+ ***************************************/
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
+
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+router.post('/register', utilities.handleErrors(accountController.registerAccount))
+
+module.exports = router
